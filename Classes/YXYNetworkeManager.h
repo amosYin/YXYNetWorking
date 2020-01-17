abac9c652b8d4a76386783ae41f951dd9ecaf781
yinxiangyu <yinxiangyu@bangbangas.com>
2020-01-17 14:10:57 +0800

    first commit

---
+++ Classes/YXYNetworkeManager.h
@@ -0,0 +1,17 @@
+//
+//  YXYNetworkeManager.h
+//  YXYNetworking_Example
+//
+//  Created by 李勇 on 2020/1/16.
+//  Copyright © 2020 1453254169@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YXYNetworkeManager : NSObject
+- (void)testPrint:(NSString *)text;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/YXYNetworkeManager.m
@@ -0,0 +1,17 @@
+//
+//  YXYNetworkeManager.m
+//  YXYNetworking_Example
+//
+//  Created by 李勇 on 2020/1/16.
+//  Copyright © 2020 1453254169@qq.com. All rights reserved.
+//
+
+#import "YXYNetworkeManager.h"
+
+@implementation YXYNetworkeManager
+- (void)testPrint:(NSString *)text {
+
+    NSLog(@"YXYNetworkManager print == %@",text);
+
+}
+@end
